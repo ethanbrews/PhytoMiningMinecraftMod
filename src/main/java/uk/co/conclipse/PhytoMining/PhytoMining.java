77087f0ffe88aea477ec0e65d7a75cb5c8efc9aa
Ethan Brews <ethanbrews77@gmail.com>
2018-03-11 16:27:37 +0000

    Init

---
+++ src/main/java/uk/co/conclipse/PhytoMining/PhytoMining.java
@@ -0,0 +1,14 @@
+package uk.co.conclipse.PhytoMining;
+
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(name=PhytoMining.MODNAME, modid=PhytoMining.MODID, version=PhytoMining.VERSION)
+public class PhytoMining {
+    public static final String MODID = "phytomining";
+    public static final String MODNAME = "Phyto Mining";
+    public static final String VERSION = "0.1";
+
+
+
+}
